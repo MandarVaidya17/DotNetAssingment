c32f3555d93c512cc7e652781734f5f11532041d
MandarVaidya17 <mandarvaidya1711@gmail.com>
2024-04-16 19:01:00 +0530

    UPDATE

---
+++ Assingments/LabAssingments/AtoZ.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//3.	Write a program to print all alphabets from a to z. - using  for loop
+namespace LabAssignment
+{
+    public class AtoZ
+    {
+        static void Main(string[] args)
+        {
+            for(char i = 'A'; i <= 'Z'; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Assingments/LabAssingments/BaseIndex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabAssignment
+{
+    internal class BaseIndex
+    {
+        static void Power(int b,int i)
+        {
+            int power=1;
+            for(int j = 1; j <= i; i++)
+            {
+            power=power*b;
+            }
+            Console.WriteLine(power);
+        }
+        static void Main(string[] args)
+        {
+            Power(2,3);
+        }
+    }
+}
